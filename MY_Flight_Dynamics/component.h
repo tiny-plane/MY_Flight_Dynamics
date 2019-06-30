4801666ed26979f88767fc0864313e5c00901f9b
weicheng di <d823036067@hotmail.com>
2019-06-30 10:37:03 +0800

    init

---
+++ MY_Flight_Dynamics/component.h
@@ -0,0 +1,49 @@
+#pragma once
+/*
+飞机外形设计和优化程序
+创建时间2019年6月29日
+创建人：邸伟承，王家琛
+///
+修改日志：
+2019年6月26日 创建文件
+
+
+
+///
+问题记录：
+
+
+
+
+///
+联系方式：
+邸伟承
+手机号：15388646168
+邮箱：823036067@qq.com
+*/
+
+#include <stdio.h>
+#include <iostream>
+#include <math.h>
+
+class component
+{
+public: ///属性
+	char name;
+	enum kind 
+	{
+		fuselage,
+		wing,
+		wing_tip,
+		h_sta,
+		v_sta
+	};
+	int slices; /// 截面数量
+	int stack;
+	int version;
+
+
+public: ///方法	
+	int get_kind(int kind_raw);
+	virtual bool Init_var(); ///各自组建重载自己的初始化方法
+};
---
+++ MY_Flight_Dynamics/main_code.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <stdio.h>
+#include <iostream>
+#include <math.h>
+#include "var.h"
+#include "component.h"
+
+
+int main();
