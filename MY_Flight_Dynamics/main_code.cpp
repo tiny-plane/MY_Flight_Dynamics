e0d682984e212590af5a9ef604620cb395459532
weicheng di <d823036067@hotmail.com>
2019-07-02 14:54:36 +0800

    平均气动弦计算完成

--- MY_Flight_Dynamics/main_code.cpp
+++ MY_Flight_Dynamics/main_code.cpp
@@ -5,7 +5,7 @@ bool Draw(void)
 {
 	section* news;
 	news = new section;
-	double pos[3] = { 0,0,0 };
+	double pos[3] = { 0.0,0.0,0.0};
 	news->Set_pos(pos);
 	news->Set_ainc(2.0);
 	news->Set_chord(2.0);
@@ -14,7 +14,7 @@ bool Draw(void)
 
 	section* news2;
 	news2 = new section;
-	double pos2[3] = { -0.5,2,0 };
+	double pos2[3] = { 0.0,2.0,0.0 };
 	news2->Set_pos(pos2);
 	news2->Set_ainc(2.0);
 	news2->Set_chord(1.0);
@@ -30,13 +30,15 @@ bool Draw(void)
 	W.append_section(*news3);
 
 	W.angle = 2;
-
+	
 	return false;
 }
 int main(int args, char argv[])
 {
 	Draw();
 	W.fresh_data();	
+	std::cout << W.get_aera() << std::endl;
+	std::cout << W.get_mac() << std::endl;
 	system("pause");
 	return 0;
 }
--- MY_Flight_Dynamics/wing.cpp
+++ MY_Flight_Dynamics/wing.cpp
@@ -23,7 +23,7 @@ double wing::cac_wing_aera(void)
 	//slices至少有1个，显示的数量就是准确的截面数量，但这里索引的时候注意。
 
 	//如果截面少于两个就不可以计算
-	std::cout << "截面数量："<< this->slices << std::endl;
+	//std::cout << "截面数量："<< this->slices << std::endl;
 	if (this->slices <= 1)
 	{
 		return 0;
@@ -31,8 +31,6 @@ double wing::cac_wing_aera(void)
 	double aera = 0;
 	//另外根截面不可以删除，默认第一个截面必须是0号截面
 	section* next = this->orisection;
-	double pos1[3] = { 0,0,0 };
-	double pos2[3] = { 0,0,0 };
 	double l1 = 0;
 	double l2 = 0;
 	double h = 0;
@@ -42,15 +40,40 @@ double wing::cac_wing_aera(void)
 		l1 = next->Get_chord() * cos(this->dec2rad(next->Get_ainc() + this->angle));
 		l2 = next->after->Get_chord() * cos(this->dec2rad(next->after->Get_ainc() + this->angle));
 		h = abs(next->Get_pos()[1] - next->after->Get_pos()[1]);
-		aera += (l1 + l2)* h / (double)2;
+		aera += (l1 + l2)* h / 2.0;
 		//std::cout <<"l1 = " <<  l1 << "  l2 =  "<< l2 << "  h =  " << h << "  aera = "<< aera << std::endl;
 		next = next->after;
 	}
-		return aera;
+		return 2.0 * aera;
 }
 bool wing::fresh_data(void)
 {
 	this->set_aera(this->cac_wing_aera());
 	this->set_mac(this->cac_wing_MAC());
 	return true;
+}
+
+double wing::cac_wing_MAC(void)
+{
+	if (this->slices < 2)
+	{
+		return 0;
+	}
+	section* next = this->orisection;
+	double l1 = 0;
+	double l2 = 0;
+	double h = 0;
+	double integration = 0;
+	double hlast = 0;
+	for (int i = 1; i < this->slices; i++)
+	{
+		//考虑扭转角和安装角的影响。
+		l1 = next->Get_chord() * cos(this->dec2rad(next->Get_ainc() + this->angle));
+		l2 = next->after->Get_chord() * cos(this->dec2rad(next->after->Get_ainc() + this->angle));
+		h = abs(next->Get_pos()[1] - next->after->Get_pos()[1]);
+		integration += (h*(l1*l1 + l1*l2 + l2*l2)) / 3;
+		next = next->after;
+	}
+
+	return 2.0 * integration / this->get_aera();
 }
